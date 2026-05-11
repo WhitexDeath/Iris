34f681d3ecf8d7865a72aa275304c135b261efbe
Daksh Tyagi <tyagidaksh737@gmail.com>
2026-05-11 20:32:42 +0530

    Fix duplicate wildcard route

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,9 +23,6 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
 
 app.get('/', (_req, res) => {
   res.json({
